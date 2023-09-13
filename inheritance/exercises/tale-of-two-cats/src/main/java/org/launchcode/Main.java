30b1c0c19027a3df131256d22450e2a7609890cc
Ronald Gutierrez <ronaldgutierrezj@gmail.com>
2023-09-13 08:20:58 -0500

    inheritance added

---
+++ inheritance/exercises/tale-of-two-cats/src/main/java/org/launchcode/Main.java
@@ -0,0 +1,11 @@
+package org.launchcode;
+
+public class Main {
+    public static void main(String[] args) {
+        HouseCat garfield = new HouseCat("Garfield", 12);
+        garfield.eat();
+        System.out.println(garfield.isTired());
+
+
+    }
+}
